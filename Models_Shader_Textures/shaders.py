c0460829c9f1e81c45ace3e37bd2d300272ec157
cor22982 <cor22982@uvg.edu.gt>
2024-08-13 02:17:35 -0600

    feat: glowshader

--- Models_Shader_Textures/shaders.py
+++ Models_Shader_Textures/shaders.py
@@ -168,3 +168,60 @@ def flatShader(**kwargs):
 	# Se regresa el color
 	return [r,g,b]
 
+def glowShader(**kwargs):
+    A, B, C = kwargs["verts"]
+    u, v, w = kwargs["bCoords"]
+    texture = kwargs["texture"]
+    dirLight = kwargs["dirLight"]
+    camMatrix = kwargs["camMatrix"]
+
+    vtA = [A[3], A[4]]
+    vtB = [B[3], B[4]]
+    vtC = [C[3], C[4]]
+
+    nA = [A[5], A[6], A[7]]
+    nB = [B[5], B[6], B[7]]
+    nC = [C[5], C[6], C[7]]
+
+    normal = [u * nA[0] + v * nB[0] + w * nC[0],
+            u * nA[1] + v * nB[1] + w * nC[1],
+            u * nA[2] + v * nB[2] + w * nC[2] ]
+
+    r = 1
+    g = 1
+    b = 1
+
+    vtP = [ u * vtA[0] + v * vtB[0] + w * vtC[0],
+        u * vtA[1] + v * vtB[1] + w * vtC[1] ]
+
+    if texture:
+        texColor = texture.getColor(vtP[0], vtP[1])
+
+    r *= texColor[0]
+    g *= texColor[1]
+    b *= texColor[2]
+
+    # intensity = normal DOT -dirlight
+    intensity = producto_punto(normal, negative_vector(dirLight) )
+    intensity = max(0, intensity)
+    r *= intensity
+    g *= intensity
+    b *= intensity 
+    #GLOW
+    yellow = [1,1,0]
+    
+    #eje z de la camara
+    camForward = [camMatrix[0][2],
+                  camMatrix[1][2],
+                  camMatrix[2][2]]
+
+    glowintensity =producto_punto(normal, camForward)
+    glowintensity = max(0, glowintensity)
+    r += yellow[0] * glowintensity
+    g += yellow[1] * glowintensity
+    b += yellow[2] * glowintensity
+    # Se regresa el color
+    return [
+        min(1, r),
+        min(1,g),
+        min(1,b)]
