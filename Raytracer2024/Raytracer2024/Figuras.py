becdceaaa90055cbe2985588ebf06cbc76147211
cor22982 <cor22982@uvg.edu.gt>
2024-09-26 22:30:45 -0600

    feat: Add Boxes and planes

--- Raytracer2024/Raytracer2024/Figuras.py
+++ Raytracer2024/Raytracer2024/Figuras.py
@@ -1,6 +1,6 @@
 import numpy as np
 from Intercept import *
-from math import atan2, acos, pi
+from math import atan2, acos, pi, isclose
 class Shape(object):
     def __init__(self, position, material):
         self.position = position
@@ -41,3 +41,164 @@ class Sphere(Shape):
         v = acos(-normal[1]) /pi
 
         return Intercept(point=P, normal=normal, distance=t0, obj=self, rayDirection=dir, texCords=[u,v])
+
+
+class Plane(Shape):
+    def __init__(self, position, material, normal):
+        super().__init__(position, material)
+        self.normal = normal
+        self.type = "Plane"
+    
+    def ray_intersect(self, origin, dir):
+        #distance = (plane Pos - rayOrigin o normal) / rayDir o nromal
+        denom = np.dot(dir, self.normal)
+
+        if isclose(0, denom):
+            return None
+        num = np.dot(np.subtract (self.position, origin), self.normal)
+
+        t = num/denom
+
+        if t < 0:
+            return None
+
+        # Puntodecontacto = origin  + dir * to
+        P = np.add(origin, np.array(dir) * t)
+        
+        return Intercept(point=P, 
+                         normal=self.normal,
+                         distance=t,
+                         texCords=None,
+                         rayDirection=dir,
+                         obj=self)
+    
+
+class Disk(Plane):
+    def __init__(self, position, material, normal, radius):
+        super().__init__(position, material, normal)
+        self.radius  = radius
+        self.type = "Disk"
+    
+    def ray_intersect(self, origin, dir):
+        planeIntercept =  super().ray_intersect(origin, dir)
+        if planeIntercept is None:
+            return None
+        contact = np.subtract(planeIntercept.point, self.position)
+        contact = np.linalg.norm(contact) # vamos a sacar la magnitud de esto
+        
+        #para ignorar todo dentro del radio
+        if contact > self.radius:
+            return None
+
+        return planeIntercept
+
+
+#estas siglas significan que son access axis aligne bunding box
+#es un cubo que no tiene rotacion, que esta alineado a x, y , z del mundo
+""
+class AABB(Shape):
+    def __init__(self, position, material, sizes):
+        super().__init__(position, material)
+        self.sizes = sizes
+        self.type = "AABB"
+        #son planos con limites
+        self.planes = []
+
+        righPlane = Plane(
+                    position=[position[0] + sizes[0]/2, position[1], position[2]], 
+                    normal=[1,0,0], 
+                    material=material)
+        lefthPlane = Plane(
+                    position=[position[0] - sizes[0]/2, position[1], position[2]], 
+                    normal=[-1,0,0], 
+                    material=material)
+        
+        UpPlane = Plane(
+                    position=[position[0], position[1] + sizes[1]/2, position[2]], 
+                    normal=[0,1,0], 
+                    material=material)
+        
+        DownPlane = Plane(
+                    position=[position[0], position[1] - sizes[1]/2, position[2]], 
+                    normal=[0,-1,0], 
+                    material=material)
+        
+        frontPlane = Plane(
+                    position=[position[0], position[1] , position[2] + sizes[2]/2], 
+                    normal=[0,0,1], 
+                    material=material)
+        
+        backPlane = Plane(
+                    position=[position[0], position[1] , position[2] + sizes[2]/2], 
+                    normal=[0,0,-1], 
+                    material=material)
+        
+        self.planes.append(righPlane)
+        self.planes.append(lefthPlane)
+        self.planes.append(UpPlane)
+        self.planes.append(DownPlane)
+        self.planes.append(frontPlane)
+        self.planes.append(backPlane)
+
+        #bounding box es una caja con limites
+
+        #Bounds
+        self.minBonds = [0,0,0]
+        self.maxBounds = [0,0,0]
+
+        #es un margen de error se podria decir
+        epsilon = 0.001
+
+        for i in range(3):
+            self.minBonds[i] = position[i] - (epsilon+sizes[i]/2)
+            self.maxBounds[i] = position[i] + (epsilon + sizes[i] / 2)
+
+
+    def ray_intersect(self, origin, dir):
+        intercept = None
+        t = float("inf")
+        for plane in self.planes:
+            planeIntercept = plane.ray_intersect(origin, dir)
+
+            if planeIntercept is not None:
+                #revisar que esta dentro de los limites
+                planePoint = planeIntercept.point
+                #si cumplo con los limites en x
+                if self.minBonds[0] <= planePoint[0] <= self.maxBounds[0]:
+                    if self.minBonds[1] <= planePoint[1] <= self.maxBounds[1]:
+                        if self.minBonds[2] <= planePoint[2] <= self.maxBounds[2]:
+                            if planeIntercept.distance < t:
+                                t = planeIntercept.distance
+                                intercept = planeIntercept
+
+        if intercept is None:
+            return None
+        
+        u, v = 0,0
+
+        if (abs(intercept.normal[0]> 0 )):
+            #mapear las uvs para el eje x usando las coordenads de y y z
+            u = (intercept.point[1]-self.minBonds[1])/self.sizes[1]
+            v = (intercept.point[2]-self.minBonds[2])/self.sizes[2]
+
+        elif (abs(intercept.normal[1]> 0 )):
+            #mapear las uvs para el eje x usando las coordenads de y y z
+            u = (intercept.point[0]-self.minBonds[0])/self.sizes[0]
+            v = (intercept.point[2]-self.minBonds[2])/self.sizes[2]
+        
+        elif (abs(intercept.normal[2]> 0 )):
+            #mapear las uvs para el eje x usando las coordenads de y y z
+            u = (intercept.point[0]-self.minBonds[0])/self.sizes[0]
+            v = (intercept.point[1]-self.minBonds[1])/self.sizes[1]
+        u = min(0.999, max(0,u))
+        v = min(0.999, max(0,v))
+        return Intercept(point=intercept.point, 
+                         normal=intercept.normal,
+                         distance=t,
+                         texCords=[u,v],
+                         rayDirection=dir,
+                         obj=self)
+
+
+
+
--- Raytracer2024/Raytracer2024/Raytracer2024.py
+++ Raytracer2024/Raytracer2024/Raytracer2024.py
@@ -6,8 +6,8 @@ from Figuras import *
 from material import *
 from Lights import *
 from texture import Texture
-width = 188
-height = 188
+width = 50
+height = 50
 
 screen = pygame.display.set_mode((width, height), pygame.SCALED )
 clock = pygame.time.Clock()
@@ -17,20 +17,24 @@ rt.enveriomentMap = Texture("Raytracer2024\Raytracer2024\parkingLot.bmp")
 rt.glClearColor(0.5,0.0,0.0)
 rt.glClear()
 #Materiales
-# brick = Material(difuse=[1,0.2,0.2], spec= 32, ks= 0.1)
+brick = Material(difuse=[1,0.2,0.2], spec= 32, ks= 0.1)
 # grass = Material(difuse=[0.2,1,0.2], spec= 64, ks= 0.2)
-# mirror = Material(difuse=[0.9,0.9,0.9], spec= 128, ks= 0.2, matType=REFLECTIVE)
+mirror = Material(difuse=[0.9,0.9,0.9], spec= 128, ks= 0.2, matType=REFLECTIVE)
 # bluemirror = Material(difuse=[0.5,0.5,1], spec= 128, ks= 0.2, matType=REFLECTIVE)
 # wood = Material(texture=Texture("Raytracer2024\Raytracer2024\lava.bmp"), spec=128, ks=0.2, matType=REFLECTIVE)
 glass = Material(ior=1.5, spec=128, ks=0.2, matType=TRANSPARENT)
-
+woodtexture = Material(texture=Texture('Raytracer2024\Raytracer2024\woodenBox.bmp'))
 
 #Lights 
 rt.lights.append(DirectionalLight(direction=[-1,-1,-1], intensity=0.8 ))
 rt.lights.append(AmbientLight(intensity=0.1))
 
 #objets
-rt.scene.append(Sphere([0,0,-5], radius=1.5, material=glass)) #la creo en todo
+#rt.scene.append(Sphere([0,0,-5], radius=1.5, material=glass)) #la creo en todo
+#rt.scene.append(Plane(position=[0,-5,-5], normal=[0,1,0], material=brick))
+#rt.scene.append(Sphere([0,0,-5], radius=1, material=brick)) #la creo en todo
+#rt.scene.append(Disk(position=[0,-1,-5], normal=[0,1,0], radius=1.5, material=mirror))
+rt.scene.append(AABB(position=[1.5,-1.5,-5], sizes=[1,1,1], material=woodtexture))
 rt.glRender()
 isRunning = True
 while isRunning:
